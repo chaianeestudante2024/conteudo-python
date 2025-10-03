73187e2f87b82c6b88f7a582f50bde8b1e48706a
CHAIANE LUISA MULLER <chaiane_muller@estudante.sesisenai.org.br>
2025-10-03 16:48:15 -0300

    Aula 2: variaveis, input e calculos

---
+++ aula2.py
@@ -0,0 +1,33 @@
+# Para criar uma variavel, colocamos:
+# o nome dela
+# o simbolo de atribuição (=)
+# o valor inicial dela
+
+# Uma variavel chamada nome que recebe o meu nome
+nome = "Chaiane"
+print("Olá, me chamo",nome)
+nome = "Clotilde"
+print("Agora meu nome é",nome)
+print("O tipo de variavel é",type(nome))
+print()
+# Uma variavel do tipo inteiro
+quantidade_alunos = 16
+print("Estamos em",quantidade_alunos,"alunos na sala")
+print("O tipo de variavel é",type(quantidade_alunos))
+print()
+# Uma variavel do tipo ...
+salario_bruto = 1425.23
+print("O salário mínimo é R$",salario_bruto)
+print("O tipo de variavel é",type(salario_bruto))
+print()
+# Uma variavel do tipo bool
+is_aberta = False
+print("A porta está aberta?",is_aberta)
+print("O tipo de variavel é",type(is_aberta))
+
+
+
+
+
+
+
